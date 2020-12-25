9e1366a1bd8043acb6284633035ee31dbf0ed9e7
Ozan Ermis <74829981+ozaneermis@users.noreply.github.com>
2020-12-25 20:34:05 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -5,3 +5,8 @@ dateofbirth = input("Please enter your birthday(Just Year)")
 list = [firstname, surname, age, dateofbirth]
 for i in list:
     print(i)
+
+if list[2] < '18':
+    print("You can't go out because it's too dangerous")
+else:
+    print("You can go out to the street")
