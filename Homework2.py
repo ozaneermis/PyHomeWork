0fe780b93763c66dd16b872edf8b760d4aa2156f
Ozan Ermis <ozaneermis@hotmail.com>
2020-12-25 20:27:43 +0300

    Homework2

---
+++ Homework2.py
@@ -0,0 +1,7 @@
+firstname = input("Please enter your first name")
+surname = input("Please enter your surname")
+age = input("Please enter your age")
+dateofbirth = input("Please enter your birthday(Just Year)")
+list = [firstname, surname, age, dateofbirth]
+for i in list:
+    print(i)
